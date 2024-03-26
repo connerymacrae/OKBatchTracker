d8b2cb3f31f7f19c55f51498afcc59ae5e21dd64
Connery MacRae <macrae.connery@gmail.com>
2024-03-26 13:04:10 -0400

    login.html
    created template and registration dirs and login.html

--- OKBatchTracker/urls.py
+++ OKBatchTracker/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('kombuchacalendar/', include('kombuchacalendar.urls')),
     path('', RedirectView.as_view(url='kombuchacalendar/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
