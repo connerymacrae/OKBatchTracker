40af22acd0a14015b0922a90857f315898e87bcf
Connery MacRae <macrae.connery@gmail.com>
2024-03-27 15:57:42 -0400

    account test setup
    mapping out tests for account app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+def test_user_can_sign_up():
+    """A user should be able to create account with username email and password"""
+
+
+def test_user_can_login():
+    """A user should be able to login with username and password"""
+
+
+def test_user_can_logout():
+    """A user should be able to logout"""
+
+
+def test_user_can_change_password():
+    """A user should be able to change password"""
+
+
+def test_user_can_reset_password():
+    """A user should be able to reset password using email"""
+    
