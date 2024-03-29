81a4cf515b73b7960e1e35b2adb547ed1e596ce0
Connery MacRae <macrae.connery@gmail.com>
2024-03-29 14:25:47 -0400

    sign-up page first tests
    Trying to get a test going that follows the function of inputting info into the sign up page and asserting that the form was submitted...

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,22 +1,43 @@
 from django.test import TestCase
-
-# Create your tests here.
-def test_user_can_sign_up():
-    """A user should be able to create account with username email and password"""
-
-
-def test_user_can_login():
-    """A user should be able to login with username and password"""
+from accounts.forms import UserRegisterForm
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
-def test_user_can_logout():
-    """A user should be able to logout"""
+class SignUpTests(TestCase):
+
+    def test_signup_view(self):
+        response = self.client.post('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_signup_form(self):
+        response = self.client.post('/accounts/signup/')
+        form = UserRegisterForm({'username': 'test',
+                                 'email': 'john@john.com',
+                                 'password1': 'XD78xd87!',
+                                 'password2': 'XD78xd87!'})
+        self.assertTrue(form.is_valid())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 1)
 
 
-def test_user_can_change_password():
-    """A user should be able to change password"""
+class AccountsTest(TestCase):
+    def setUp(self):
+        test_user = User.objects.create_user({'username': 'johnjohn',
+                                              'email': 'john@john.com',
+                                              'password1': 'XD78xd87!',
+                                              'password2': 'XD78xd87!'})
+        test_user.save()
 
+    def test_user_can_login(self):
+        """A user should be able to log in with username and password"""
 
-def test_user_can_reset_password():
-    """A user should be able to reset password using email"""
-    
+    def test_user_can_logout(self):
+        """A user should be able to log out"""
+
+    def test_user_can_change_password(self):
+        """A user should be able to change password"""
+
+    def test_user_can_reset_password(self):
+        """A user should be able to reset password using email"""
