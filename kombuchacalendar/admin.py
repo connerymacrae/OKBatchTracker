84e95794df7bdad2c636c83be78439bd7b151541
Connery MacRae <macrae.connery@gmail.com>
2024-06-10 18:10:43 -0400

    Batch model admin update

--- kombuchacalendar/admin.py
+++ kombuchacalendar/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Batch
+
+@admin.register(Batch)
+class BatchAdmin(admin.ModelAdmin):
+    list_display = ('brewer', 'name' 'description', 'date_brewed', 'starter_type')
