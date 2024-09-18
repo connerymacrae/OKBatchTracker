5316302f50c12e9e4a9f349c2b71424728dc048d
Connery MacRae <macrae.connery@gmail.com>
2024-09-18 15:52:30 -0400

    working on login still....

--- accounts/views.py
+++ accounts/views.py
@@ -12,23 +12,47 @@ class SignUpView(CreateView):
     success_url = reverse_lazy("kombuchacalendar:index")
     template_name = "registration/signup.html"
 
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
+    def register(self, request):
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                new_user = form.save()
+                messages.info(request, "Thanks for signing up. You are now logged in.")
+                new_user = authenticate(username=form.cleaned_data['username'],
+                                        password=form.cleaned_data['password1'],
+                                        )
+                login(request, new_user)
+                return redirect(self.success_url)
+        else:
+            form = self.form_class(initial=self.initial)
+            return render(request, self.template_name, {'form': form})
 
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
 
-        if form.is_valid():
-            form.save()
 
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
+    # def form_valid(self, form):
+    #     valid = super(SignUpView, self).form_valid(form)
+    #     email, password = form.cleaned_data.get('email'), form.cleaned_data.get('password1')
+    #     user = authenticate(email=email, password=password)
+    #     login(self.request, user)
+    #     return valid
 
-            return redirect(reverse_lazy('kombuchacalendar:index'))
-
-        return render(request, self.template_name, {"form": form})
-
+    # def get(self, request, *args, **kwargs):
+    #     form = self.form_class(initial=self.initial)
+    #     return render(request, self.template_name, {'form': form})
+    #
+    # def post(self, request, *args, **kwargs):
+    #     form = self.form_class(request.POST)
+    #
+    #     if form.is_valid():
+    #         form.save()
+    #
+    #         username = form.cleaned_data.get('username')
+    #         messages.success(request, f'Account created for {username}')
+    #
+    #         return redirect(reverse_lazy('kombuchacalendar:index'))
+    #
+    #     return render(request, self.template_name, {"form": form})
+    #
     # def form_valid(self, form):
     #     # create user object
     #     user = form.save(commit=False)
